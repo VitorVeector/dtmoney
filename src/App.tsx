8a1613442cc5b19f6dd23dc1d2fb5e28743d119f
Vitor Hugo <job_vitoraraujo@outlook.com>
2022-03-09 19:35:32 -0300

    configuração padrão do styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  color: #8257e6;
-  font-size: 4rem;
-  button{
-    padding: 2rem;
-    color: red
-  }
-` 
+import {GlobalStyle} from './styles/global'
 
 function App() {
   return (
     <div className="App">
-      <Title>
-        Hello World
-        <button>Alerta vermelho</button>
-      </Title>
+      <h1>Hello World</h1>
+      <GlobalStyle/>
     </div>
   );
 }
