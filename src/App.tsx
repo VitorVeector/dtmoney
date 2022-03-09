378d07950c3d2ee410349dac7dc43f3e31dadffc
Vitor Hugo <job_vitoraraujo@outlook.com>
2022-03-09 16:02:04 -0300

    Entendendo styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 4rem;
+  button{
+    padding: 2rem;
+    color: red
+  }
+` 
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>
+        Hello World
+        <button>Alerta vermelho</button>
+      </Title>
     </div>
   );
 }
 
-export default App;
+export default App
+
+
